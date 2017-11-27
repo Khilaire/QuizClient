276bb6f71f57d2767f18422a22347a6f84554b06
kevinhilaire <kehi14ab@student.cbs.dk>
2017-11-27 12:56:15 +0000

    Mindre tilføjelser - indikatorer for kommende kode

--- js/login.js
+++ js/login.js
@@ -1,30 +1,31 @@
 $(document).ready(() => {
 
-  SDK.User.loadNav();
+    SDK.User.loadNav();
 
-  $("#login-button").click(() => {
+    $("#login-button").click(() => {
 
-    const username = $("#inputUsername").val();
-    const password = $("#inputPassword").val();
+        const username = $("#inputUsername").val();
+        const password = $("#inputPassword").val();
 
-    SDK.User.login(username, password, (err, data) => {
-      if (err && err.xhr.status === 401) {
-        $(".form-group").addClass("has-error");
-      }
-      else if (err){
-        console.log("BAd stuff happened")
-      } else {
+        // Inspiration fra tutor projekt
+        SDK.User.login(username, password, (err, data) => {
+            if (err && err.xhr.status === 401) {
+                $(".form-group").addClass("has-error");
+            }
+            else if (err){
+                console.log("Something wrong happened")
+            } else {
 
-        if(data.type === 1) {
-          window.location.href = "user-page.html"
-        } else {
-          window.location.href = "admin-page.html"
-            console.log(data)
-        }
+                if(data.type === 1) {
+                    window.location.href = "user-page.html"
+                } else {
+                    window.location.href = "admin-page.html"
+                    console.log(data)
+                }
 
-      }
+            }
+        });
+
     });
-
-  });
 
 });
