b5102655f6fe8f8b9b4534898b18a6854a431808
kevinhilaire <kehi14ab@student.cbs.dk>
2017-12-20 06:09:44 +0100

    Opret administrator

--- js/signup.js
+++ js/signup.js
@@ -2,16 +2,17 @@ $(document).ready(() => {
 
     SDK.User.loadNavNon();
 
+    // Opret default bruger
     $("#createUserbtn").on("click", () => {
 
         const firstName = $("#inputName").val();
         const surname = $("#inputSurname").val();
-        const userName = $("#inputUsername").val();
+        const username = $("#inputUsername").val();
         const password = $("#inputPassword").val();
         const type = 1; // man kan kun oprette en admin (type 0) såfremt man selv er logget ind som admin. Derfor er type per default altid 1.
 
-        SDK.User.createUser(firstName, surname, userName, password, type, (err, data) => {
-            console.log(firstName + surname + userName + password + type);
+        SDK.User.createUser(firstName, surname, username, password, type, (err, data) => {
+            console.log(firstName + surname + username + password + type);
 
 
             if (err && err.xhr.status === 401) {
@@ -21,12 +22,42 @@ $(document).ready(() => {
                 console.log("Der er sket en fejl!")
             } else {
 
-                if (!userName || !password) {
+                if (!username || !password) {
                     window.alert("Indtast venligst brugernavn og kodeord!");
 
                 } else {
                     alert("Tillykke " + firstName + ", din bruger er blevet oprettet!");
                     window.location.href = ("loginPage.html");
+                }
+            }
+        });
+    });
+
+    $("#createAdmbtn").on("click", () => {
+
+        const firstName = $("#inputName").val();
+        const surname = $("#inputSurname").val();
+        const username = $("#inputUsername").val();
+        const password = $("#inputPassword").val();
+        const type = 0;
+
+        SDK.User.createUser(firstName, surname, username, password, type, (err, data) => {
+            console.log(firstName + surname + username + password + type);
+
+
+            if (err && err.xhr.status === 401) {
+                console.log(data)
+            }
+            else if (err) {
+                console.log("Der er sket en fejl!")
+            } else {
+
+                if (!username || !password) {
+                    window.alert("Indtast venligst brugernavn og kodeord!");
+
+                } else {
+                    alert("Succes! " + firstName + " " + surname + " er oprettet som administrator!");
+                    window.location.href = ("adminPage.html");
                 }
             }
         });
