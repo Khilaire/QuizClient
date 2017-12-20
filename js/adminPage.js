e81ec67dcb673e6e9a0f2b5056ef2a83e414ffc4
kevinhilaire <kehi14ab@student.cbs.dk>
2017-12-20 08:24:40 +0100

    Opret quiz + spørgsmål + muligheder + svar

--- js/adminPage.js
+++ js/adminPage.js
@@ -1,12 +1,12 @@
 $(document).ready(() => {
 
     SDK.User.loadNavAdmin();
+    let quizTitle = $("#inputQuizTitle").val();
+    let courseId = $(".inputSelectCourse").find("option:selected").val();
+    let quizId = SDK.getQueryParam("quizId");
 
-    // Opret quiz
+    // Opret ny quiz
     $("#createQuizBtn").click(() => {
-        var quizTitle = $("#inputQuizTitle").val();
-        var courseId = $(".inputSelectCourse").find("option:selected").val();
-
 
         SDK.Quiz.createQuiz(courseId, quizTitle, (err, data) => {
             console.log(err);
@@ -14,14 +14,11 @@ $(document).ready(() => {
             if (err && err.xhr.status === 401) {
                 console.log(data);
             }
-
             else if (err) {
                 console.log("Der er sket en fejl!")
-
             } else {
                 alert("Succes! Din quiz, " + quizTitle + ", er nu oprettet")
             }
         });
     });
-
 });
